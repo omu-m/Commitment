72ae670fa7cfbff996a78b96b1bb965e795cb0fc
MoekaOmura <110664281+omu-m@users.noreply.github.com>
2022-10-07 11:48:34 +0000

    model

---
+++ db/migrate/20221007113822_create_subtasks.rb
@@ -0,0 +1,17 @@
+class CreateSubtasks < ActiveRecord::Migration[6.1]
+  def change
+    create_table :subtasks do |t|
+
+      # 親タスクIDを保存するカラム
+      t.integer :task_id, null: false
+      # 会員IDを保存するカラム
+      t.integer :member_id, null: false
+      # 子タスクの内容を保存するカラム
+      t.text :subtask, null: false
+      # enumで管理　integer型の進捗ステータスカラム
+      t.integer :progress_status,null: false, default: 0
+
+      t.timestamps null: false
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_07_051330) do
+ActiveRecord::Schema.define(version: 2022_10_07_113822) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -30,10 +30,38 @@ ActiveRecord::Schema.define(version: 2022_10_07_051330) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "name", null: false
+    t.string "nickname", null: false
+    t.boolean "is_active", default: true, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_members_on_email", unique: true
     t.index ["reset_password_token"], name: "index_members_on_reset_password_token", unique: true
+  end
+
+  create_table "subtasks", force: :cascade do |t|
+    t.integer "task_id", null: false
+    t.integer "member_id", null: false
+    t.text "subtask", null: false
+    t.integer "progress_status", default: 0, null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "task_members", force: :cascade do |t|
+    t.integer "task_id", null: false
+    t.integer "member_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "tasks", force: :cascade do |t|
+    t.integer "member_id", null: false
+    t.string "task_title", null: false
+    t.text "task", null: false
+    t.float "achievement_rate", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
