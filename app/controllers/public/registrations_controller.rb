367693784c7c3eb4af93975ba1a9ef61e7c4ee7f
MoekaOmura <110664281+omu-m@users.noreply.github.com>
2022-10-23 12:00:54 +0000

    public/all

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -52,7 +52,7 @@ class Public::RegistrationsController < Devise::RegistrationsController
   # If you have extra params to permit, append them to the sanitizer.
 
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:display_name, :user_name, :email])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:display_name, :user_name, :email, :profile_image])
   end
 
 
