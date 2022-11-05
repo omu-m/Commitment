ef359489f458fe41e2c131e6dabe1d0b4cabc0b9
MoekaOmura <110664281+omu-m@users.noreply.github.com>
2022-11-05 14:24:04 +0000

    task：修正

--- app/models/task_member.rb
+++ app/models/task_member.rb
@@ -4,6 +4,6 @@ class TaskMember < ApplicationRecord
   belongs_to :task
 
   # 承認ステータス
-  enum approval_status: { approval_pending: 0, approval: 1, non_approval: 2 }
+  enum approval_status: { before_participation:0, approval_pending: 1, approval: 2, denial_by_owner: 3, denial_by_member: 4 }
 
 end
