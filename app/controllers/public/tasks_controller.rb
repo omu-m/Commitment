404c86ebca0c1a15515f0c1fadd9bc7ca437edd3
MoekaOmura <110664281+omu-m@users.noreply.github.com>
2022-11-23 13:37:57 +0000

    all：改善

--- app/controllers/public/tasks_controller.rb
+++ app/controllers/public/tasks_controller.rb
@@ -52,9 +52,10 @@ class Public::TasksController < ApplicationController
   def show
     @task = Task.find(params[:id])
     @tasknew = Task.new
-    @task_member = @task.task_members.find_by(member: current_member)
-    @status = @task_member.present? ? @task_member.approval_status_before_type_cast : 0
-    @current_status = @task_member.present? ? @task_member.approval_status_i18n : "参加前"
+    # 条件を指定して初めの1件を取得し1件もなければ作成
+    @task_member = @task.task_members.find_or_initialize_by(member: current_member)
+    # @approval_status = @task_member.present? ? @task_member.approval_status : "before_participation"
+    # @current_approval_status = @task_member.present? ? @task_member.approval_status_i18n : "参加前"
   end
 
   # 参加
@@ -66,7 +67,7 @@ class Public::TasksController < ApplicationController
 
   # 退出
   def out
-    @task = Task.find(params[:task_id])
+    @task = Task.find(params[:id])
     @task.members.delete(current_member)
     redirect_to  tasks_path
   end
@@ -92,7 +93,7 @@ class Public::TasksController < ApplicationController
 
   def destroy
     # @task = Task.find(params[:id])
-    task_member = TaskMember.find_by(task_id: params[:id], member_id: current_member.id)
+    task_member = current_member.task_members.find_by(task_id: params[:id])
     task_member.destroy
     # # current_memberは、@task.membersから消されるという記述。
     # @task.members.delete(current_member)
@@ -100,14 +101,14 @@ class Public::TasksController < ApplicationController
   end
 
   def destroy_all
-    @task = Task.find(params[:task_id])
+    @task = Task.find(params[:id])
     if @task.destroy
     redirect_to tasks_path
     end
   end
 
   def request_join
-    task = Task.find(params[:task_id])
+    task = Task.find(params[:id])
     # ログイン中のユーザーが、特定のタスクに参加申請を行う
     task_member = task.task_members.find_by(member_id: current_member.id)
     if task_member.present?
@@ -115,7 +116,7 @@ class Public::TasksController < ApplicationController
         task_member.approval_pending!
       end
     else
-      task.task_members.create!(member_id: current_member.id, approval_status: 1)
+      task.task_members.create!(member_id: current_member.id, approval_status: "approval_pending")
     end
     # 通知
     task.create_activities(task, "request_join", current_member.id, task.owner_id)
@@ -124,8 +125,9 @@ class Public::TasksController < ApplicationController
   end
 
   def request_join_destroy
-    TaskMember.find_by(task_id: params[:task_id], member_id: current_member.id).update(approval_status: 4)
-    @task = Task.find(params[:task_id])
+    task_member = TaskMember.find_by!(task_id: params[:id], member_id: current_member.id)
+    task_member.denial_by_member!
+    @task = Task.find(params[:id])
     # 通知
     @task.create_activities(@task, "request_join_destroy", current_member.id, @task.owner_id)
     # タスク詳細ページに戻る
@@ -133,34 +135,34 @@ class Public::TasksController < ApplicationController
   end
 
   def applies
-    @task = Task.find(params[:task_id])
-    @task_members = @task.task_members.where(approval_status: 1)
+    @task = Task.find(params[:id])
+    @task_members = @task.task_members.where(approval_status: "approval_pending")
   end
 
   def leaving
-    TaskMember.find_by(task_id: params[:task_id], member_id: params[:member_id]).update(approval_status: 5)
+    TaskMember.find_by(task_id: params[:id], member_id: params[:member_id]).update(approval_status: "leaving")
     # 通知
-    @task = Task.find(params[:task_id])
+    @task = Task.find(params[:id])
     @task.create_activities(@task, "leaving", @task.owner_id, params[:member_id])
-    redirect_to  task_path(params[:task_id])
+    redirect_to  task_path(params[:id])
   end
 
   def approval_request
     # オーナーがログイン中のユーザーが、特定のタスクに参加する承認を行う
-    TaskMember.find_by(task_id: params[:task_id], member_id: params[:member_id]).update(approval_status: 2)
+    TaskMember.find_by(task_id: params[:id], member_id: params[:member_id]).update(approval_status: "approval")
     # 通知
-    @task = Task.find(params[:task_id])
+    @task = Task.find(params[:id])
     @task.create_activities(@task, "approval_request", @task.owner_id, params[:member_id])
-    redirect_to task_applies_path(params[:task_id])
+    redirect_to applies_task_path(params[:id])
   end
 
   def non_approval_request
     # オーナーがログイン中のユーザーが、特定のタスクに参加する非承認を行う
-    TaskMember.find_by(task_id: params[:task_id], member_id: params[:member_id]).update(approval_status: 3)
+    TaskMember.find_by(task_id: params[:id], member_id: params[:member_id]).update(approval_status: "denial_by_owner")
     # 通知
-    @task = Task.find(params[:task_id])
+    @task = Task.find(params[:id])
     @task.create_activities(@task, "non_approval_request", @task.owner_id, params[:member_id])
-    redirect_to task_applies_path(params[:task_id])
+    redirect_to applies_task_path(params[:id])
   end
 
   private
